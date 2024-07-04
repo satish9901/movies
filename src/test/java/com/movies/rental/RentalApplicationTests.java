93dfb1ce97c6ddd90cca21b0a5a94c5669e55e02
satish9901 <satishgudurukc@gmail.com>
2024-07-04 09:06:57 -0400

    Initial commit

---
+++ src/test/java/com/movies/rental/RentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movies.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
