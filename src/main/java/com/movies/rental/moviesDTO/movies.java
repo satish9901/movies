d27c5ef20418fc3bcce2854e34e255d686987d50
satish9901 <satishgudurukc@gmail.com>
2024-07-04 10:42:43 -0400

    step 1

---
+++ src/main/java/com/movies/rental/moviesDTO/movies.java
@@ -0,0 +1,11 @@
+package com.movies.rental.moviesDTO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class movies {
+    public static void main(String[] args){
+        String sql = "select * from actor";
+    }
+}
